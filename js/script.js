903500c2686264ed3081a1c65db263b248198ade
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-06-11 12:47:09 +0200

    Add mustache templates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
+'use strict';
+(function(){ 
+
+  //Mustache templates 
+  var imageList = document.getElementById('image-list');
+  var imageItem = document.getElementById('template-image').innerHTML;
+
+Mustache.parse(imageItem);
+
+var imageListHTML = '';
+
+for (var i=0; i < imageData.length; i++ ){
+  console.log(imageData);
+  imageListHTML += Mustache.render(imageItem, imageData[i]);
+}
+
+imageList.insertAdjacentHTML('beforeend', imageListHTML);
+
+// Flickity carousel
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
   pageDots: false,
-  hash: true
+  //hash.js script added for this to work
+  hash: true 
 });
 
+//restart button
 document.querySelector('.restart').addEventListener('click', function(){
   flkty.selectCell(0);
 });
-
-var progressBar = document.querySelector('.progress-bar')
-
+//progress bar
+var progressBar = document.querySelector('.progress-bar');
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+})();
