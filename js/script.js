cbc89ff71786714e5b1ee496f490ac9b97f3cb08
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-06-11 09:46:21 +0200

    Modify style and index.html

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ var flkty = new Flickity( elem, {
   pageDots: false,
   hash: true
 });
+
+document.querySelector('.restart').addEventListener('click', function(){
+  flkty.selectCell(0);
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
